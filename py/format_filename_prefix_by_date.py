97deb8ad5b811b46deecada64078c1b481e590bb
Ardent Illumina <195614466+ardentillumina@users.noreply.github.com>
2025-02-06 23:06:01 +0200

    update: FormatFilenamePrefixByDate

--- py/format_filename_prefix_by_date.py
+++ py/format_filename_prefix_by_date.py
@@ -20,19 +20,26 @@ class FormatFilenamePrefixByDate(BaseNodeStringAssistant):
                 "suffix": ("STRING", {
                     "default": "",
                 }),
+                "enable_date_folder": ("BOOLEAN", {
+                    "default": True,
+                }),
             },
         }
 
-    def func(self, seed=None, suffix=""):
+    def func(self, seed=None, suffix="", enable_date_folder=True):
         current_time = datetime.datetime.now()
         formatted_date = current_time.strftime("%Y%m%d")
         formatted_date_and_time = current_time.strftime("%Y%m%d_%H%M")
 
         fragments: list[str] = []
-        fragments.append(os.path.join(
-            formatted_date,
-            formatted_date_and_time,
-        ))
+
+        if enable_date_folder:
+            fragments.append(os.path.join(
+                formatted_date,
+                formatted_date_and_time,
+            ))
+        else:
+            fragments.append(formatted_date_and_time)
 
         if seed is not None:
             fragments.append(f"_{seed}")
